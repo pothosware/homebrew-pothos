33d494546ce7e72eddfd75b772d888c399dde6e0
Josh Blum <josh@joshknows.com>
2015-10-10 13:23:07 -0700

    soapysdr /usr/local search patch

--- soapysdr.rb
+++ soapysdr.rb
@@ -5,6 +5,11 @@ class Soapysdr < Formula
   depends_on "cmake" => :build
   depends_on "swig" => :build
 
+  #This patch forces SoapySDR to search the /usr/local prefix for modules.
+  #This patch is needed because the CMAKE_INSTALL_PREFIX is not /usr/local.
+  #Another solution would be to set SOAPY_SDR_ROOT environment variable.
+  patch :DATA
+
   def install
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
@@ -12,3 +17,18 @@ class Soapysdr < Formula
     end
   end
 end
+
+__END__
+diff --git a/lib/Modules.in.cpp b/lib/Modules.in.cpp
+index 8965f85..09e1b83 100644
+--- a/lib/Modules.in.cpp
++++ b/lib/Modules.in.cpp
+@@ -95,7 +95,7 @@ std::vector<std::string> SoapySDR::listModules(void)
+     searchPaths.push_back(SoapySDR::getRootPath() + "/lib@LIB_SUFFIX@/SoapySDR/modules");
+ 
+     //support /usr/local module installs when the install prefix is /usr
+-    if (SoapySDR::getRootPath() == "/usr")
++    //if (SoapySDR::getRootPath() == "/usr")
+     {
+         searchPaths.push_back("/usr/local/lib@LIB_SUFFIX@/SoapySDR/modules");
+         //when using a multi-arch directory, support single-arch path as well
