5780d9bc1cc2faa29add444bc189071c90b38322
Josh Blum <josh@joshknows.com>
2015-10-11 11:30:29 -0700

    update soapysdr for 0.3.1 tag

--- soapysdr.rb
+++ soapysdr.rb
@@ -2,40 +2,19 @@ class Soapysdr < Formula
   desc "Vendor and platform neutral SDR support library"
   homepage "https://github.com/pothosware/SoapySDR/wiki"
   head "https://github.com/pothosware/SoapySDR.git"
-  url "https://github.com/pothosware/SoapySDR/archive/soapy-sdr-0.3.0.tar.gz"
-  sha256 "8a0c2b7bf0f5dbe55d8f99f2f73e734e03f01f4d40307cd1da70575a4d2763b7"
+  url "https://github.com/pothosware/SoapySDR/archive/soapy-sdr-0.3.1.tar.gz"
+  sha256 "f631235c0caf94faab17c05c98cfbf8f41ecb1ce86e9c8d790be9218943ebed4"
 
   depends_on "cmake" => :build
   depends_on "swig" => :build
 
-  ### TODO ###
-  #
-  # when 0.3.1 is tagged, pass SOAPY_SDR_ROOT=#{HOMEBREW_PREFIX} and remove this patch...
-  #
-  #This patch forces SoapySDR to search the /usr/local prefix for modules.
-  #This patch is needed because the CMAKE_INSTALL_PREFIX is not /usr/local.
-  #Another solution would be to set SOAPY_SDR_ROOT environment variable.
-  patch :DATA
-
   def install
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      args = %W[
+        -DSOAPY_SDR_ROOT='#{HOMEBREW_PREFIX}'
+      ] + std_cmake_args
+      system "cmake", "..", *args
       system "make", "install"
     end
   end
 end
-
-__END__
-diff --git a/lib/Modules.in.cpp b/lib/Modules.in.cpp
-index 8965f85..09e1b83 100644
---- a/lib/Modules.in.cpp
-+++ b/lib/Modules.in.cpp
-@@ -95,7 +95,7 @@ std::vector<std::string> SoapySDR::listModules(void)
-     searchPaths.push_back(SoapySDR::getRootPath() + "/lib@LIB_SUFFIX@/SoapySDR/modules");
- 
-     //support /usr/local module installs when the install prefix is /usr
--    if (SoapySDR::getRootPath() == "/usr")
-+    //if (SoapySDR::getRootPath() == "/usr")
-     {
-         searchPaths.push_back("/usr/local/lib@LIB_SUFFIX@/SoapySDR/modules");
-         //when using a multi-arch directory, support single-arch path as well
