365500961809a2e015c5d5ad32dcb18e0d07939f
Josh Blum <josh@joshknows.com>
2015-12-09 20:35:25 -0800

    added serialization release + sha256

--- pothosserialization.rb
+++ pothosserialization.rb
@@ -1,7 +1,9 @@
 class Pothosserialization < Formula
   desc "Pothos serialization support software"
   homepage "https://github.com/pothosware/pothos-serialization/blob/master/README.md"
-  head "https://github.com/pothosware-serialization/pothos.git"
+  head "https://github.com/pothosware/pothos-serialization.git"
+  url "https://github.com/pothosware/pothos-serialization/archive/pothos-serialization-0.2.0.tar.gz"
+  sha256 "a75842a33d94182795f9ed2862d0f9adac01377b3c71ee7fd9a6220133f44491"
 
   depends_on "cmake" => :build
 
