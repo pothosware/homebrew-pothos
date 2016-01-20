ebb76d9b2035af1be4244a76b4e03fef5417e8f8
Josh Blum <josh@joshknows.com>
2016-01-19 17:30:22 -0800

    soapy-redpitaya-0.1.0 release

--- soapyredpitaya.rb
+++ soapyredpitaya.rb
@@ -2,6 +2,8 @@ class Soapyredpitaya < Formula
   desc "Soapy SDR plugin for Red Pitaya"
   homepage "https://github.com/pothosware/SoapyRedPitaya/wiki"
   head "https://github.com/pothosware/SoapyRedPitaya.git"
+  url "https://github.com/pothosware/SoapyRedPitaya/archive/soapy-redpitaya-0.1.0.tar.gz"
+  sha256 "5cfe992708491ade06947c874cada70cc6d91f18b1fecdee24eca8ef23234bad"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
