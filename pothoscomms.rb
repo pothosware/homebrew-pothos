367d32a081228259b7ee90452dd59180b7d84f98
Josh Blum <josh@joshknows.com>
2016-01-29 12:08:17 -0800

    pothos-comms-0.1.2 tag

--- pothoscomms.rb
+++ pothoscomms.rb
@@ -2,8 +2,8 @@ class Pothoscomms < Formula
   desc "Pothos communications blocks"
   homepage "https://github.com/pothosware/pothos-comms/wiki"
   head "https://github.com/pothosware/pothos-comms.git"
-  url "https://github.com/pothosware/pothos-comms/archive/pothos-comms-0.1.1.tar.gz"
-  sha256 "848c82e2e014436fd145f78cd5c91e2c77c53f26ce045343d7f1312f7f017d53"
+  url "https://github.com/pothosware/pothos-comms/archive/pothos-comms-0.1.2.tar.gz"
+  sha256 "e32e4e1d7b0824ea257963c52114fe051d5f496de60174b6cf02198978513221"
 
   depends_on "cmake" => :build
   depends_on "pothos"
