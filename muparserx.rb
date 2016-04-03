a6e3d441e7d3558f4309c136eb8ceab765bc8835
Josh Blum <josh@joshknows.com>
2016-04-03 15:28:11 -0700

    muparserx v4.0.7 tag

--- muparserx.rb
+++ muparserx.rb
@@ -2,6 +2,8 @@ class Muparserx < Formula
   desc "the muparserx math parser library"
   homepage "http://articles.beltoforion.de/article.php?a=muparserx"
   head "https://github.com/beltoforion/muparserx.git"
+  url "https://github.com/beltoforion/muparserx/archive/v4.0.7.tar.gz"
+  sha256 "dd3c68da70a7177224fba015de8a948f2c8e6940d3c6ecde1a87d87ed97d6edf"
 
   depends_on "cmake" => :build
 
