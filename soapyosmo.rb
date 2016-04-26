7a7586465cc7b29af3df65e1086ce1e8911cf6d9
Josh Blum <josh@joshknows.com>
2016-04-26 12:13:48 -0400

    soapy-osmo-0.2.3

--- soapyosmo.rb
+++ soapyosmo.rb
@@ -2,8 +2,8 @@ class Soapyosmo < Formula
   desc "Soapy SDR plugins for OsmoSDR devices"
   homepage "https://github.com/pothosware/SoapyOsmo/wiki"
   head "https://github.com/pothosware/SoapyOsmo.git"
-  url "https://github.com/pothosware/SoapyOsmo/archive/soapy-osmo-0.2.2.tar.gz"
-  sha256 "0edf3c56177c403190fcc9f47926cb80c2051cbf10de52be2608731a6991e548"
+  url "https://github.com/pothosware/SoapyOsmo/archive/soapy-osmo-0.2.3.tar.gz"
+  sha256 "f596ac2ef9b8b9bb107404b7d7748784880c6ca1c2c9d3dfb127bbbb01efd523"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
