e76fa028469b7095f44e705bc03c8e82813d3748
Josh Blum <josh@joshknows.com>
2016-05-20 10:02:15 -0700

    soapy-bladerf-0.3.2 tag

--- soapybladerf.rb
+++ soapybladerf.rb
@@ -2,8 +2,8 @@ class Soapybladerf < Formula
   desc "Soapy SDR plugin for Blade RF"
   homepage "https://github.com/pothosware/SoapyBladeRF/wiki"
   head "https://github.com/pothosware/SoapyBladeRF.git"
-  url "https://github.com/pothosware/SoapyBladeRF/archive/soapy-bladerf-0.3.1.tar.gz"
-  sha256 "d9493662361a6bb8de3e39c7d15438829912b4bb297bfa055b5894ace5d9881c"
+  url "https://github.com/pothosware/SoapyBladeRF/archive/soapy-bladerf-0.3.2.tar.gz"
+  sha256 "e97d849ab80dfe3142c072a0709693184db9b521e17241882f47f93744d762d7"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
