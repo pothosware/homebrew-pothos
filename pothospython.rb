ed97a1881af17bcdac73d9bb4513c884019f6e35
Josh Blum <josh@joshknows.com>
2016-06-24 16:28:16 -0700

    pothos-python-0.1.4 tag

--- pothospython.rb
+++ pothospython.rb
@@ -2,8 +2,8 @@ class Pothospython < Formula
   desc "Pothos language bindings for Python"
   homepage "https://github.com/pothosware/pothos-python/wiki"
   head "https://github.com/pothosware/pothos-python.git"
-  url "https://github.com/pothosware/pothos-python/archive/pothos-python-0.1.3.tar.gz"
-  sha256 "5117c010b23389dc04951a830a419f790c381060d32ce31aaadb71943c51db54"
+  url "https://github.com/pothosware/pothos-python/archive/pothos-python-0.1.4.tar.gz"
+  sha256 "df4863194171d39dec320018cf27c45ae40d8b877f2915440e38d822e2ed4d7e"
 
   depends_on "cmake" => :build
   depends_on "pothos"
