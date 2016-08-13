990f0c4d76c4b5e06ffcd81f7a5f50e333076bd1
Josh Blum <josh@joshknows.com>
2016-08-13 09:31:05 -0700

    soapy-uhd-0.3.1

--- soapyuhd.rb
+++ soapyuhd.rb
@@ -2,8 +2,8 @@ class Soapyuhd < Formula
   desc "Soapy SDR plugins for UHD devices"
   homepage "https://github.com/pothosware/SoapyUHD/wiki"
   head "https://github.com/pothosware/SoapyUHD.git"
-  url "https://github.com/pothosware/SoapyUHD/archive/soapy-uhd-0.3.0.tar.gz"
-  sha256 "db862bab9a6d7b08b24fabbb4f68d21b7d642f2b5762aa99f7ccd5e4114a7697"
+  url "https://github.com/pothosware/SoapyUHD/archive/soapy-uhd-0.3.1.tar.gz"
+  sha256 "323f8ea872eafd174e619aa6d71531dd818dbccdbabaa59d391278325f20cdb5"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
