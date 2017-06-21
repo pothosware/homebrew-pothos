2bf7a6a79933e48802250affca860829a3d95a91
Josh Blum <josh@joshknows.com>
2017-06-20 21:49:56 -0500

    limesuite tag v17.06.0

--- limesuite.rb
+++ limesuite.rb
@@ -2,8 +2,8 @@ class Limesuite < Formula
   desc "Lime suite device drivers, GUI, and SDR support"
   homepage "https://github.com/myriadrf/LimeSuite/blob/master/README.md"
   head "https://github.com/myriadrf/LimeSuite.git"
-  url "https://github.com/myriadrf/LimeSuite/archive/v17.02.2.tar.gz"
-  sha256 "dcccddca98a97950aac07dd903fc840ff28a9ba255e0baf11c58b2524540520b"
+  url "https://github.com/myriadrf/LimeSuite/archive/v17.06.0.tar.gz"
+  sha256 "615014ec946bfa837877d4c9e6968d44446a55f3357f53d713ffa3a7b2ed428e"
 
   depends_on "cmake" => :build
   depends_on "sqlite" #core library dependency
