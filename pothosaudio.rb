d5f133ac6d2975398011cb943f237851e3330079
Josh Blum <josh@joshknows.com>
2017-07-16 14:44:49 -0500

    pothos-audio-0.2.1

--- pothosaudio.rb
+++ pothosaudio.rb
@@ -2,8 +2,8 @@ class Pothosaudio < Formula
   desc "Pothos audio source and sink blocks"
   homepage "https://github.com/pothosware/pothos-audio/wiki"
   head "https://github.com/pothosware/pothos-audio.git"
-  url "https://github.com/pothosware/pothos-audio/archive/pothos-audio-0.2.0.tar.gz"
-  sha256 "0a39b062fa101dc86cc4d5b7dee89a2c2f108f1fe493cc865f26da76e966474c"
+  url "https://github.com/pothosware/pothos-audio/archive/pothos-audio-0.2.1.tar.gz"
+  sha256 "350e26f926232368ed47cae19ad556db5f491c09285c0bb2cf3d040c7d535a29"
 
   depends_on "cmake" => :build
   depends_on "pothos"
