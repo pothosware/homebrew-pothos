d4dc6bd4903af4cbc4c11ae3633f48e0dd7ed779
Josh Blum <josh@joshknows.com>
2017-07-22 21:54:18 -0500

    pothos-sdr-0.4.4 tag

--- pothossdr.rb
+++ pothossdr.rb
@@ -2,8 +2,8 @@ class Pothossdr < Formula
   desc "Pothos SDR source and sink blocks"
   homepage "https://github.com/pothosware/pothos-sdr/wiki"
   head "https://github.com/pothosware/pothos-sdr.git"
-  url "https://github.com/pothosware/pothos-sdr/archive/pothos-sdr-0.4.3.tar.gz"
-  sha256 "aaa78d7906dad8e51bd8395ebd4a7f3e87318264968dc9c8869c500462f4439d"
+  url "https://github.com/pothosware/pothos-sdr/archive/pothos-sdr-0.4.4.tar.gz"
+  sha256 "cd6e15b8025c984c0463c1620a942bc35bca2e813113dda3bf2a116bed7afeb9"
 
   depends_on "cmake" => :build
   depends_on "pothos"
