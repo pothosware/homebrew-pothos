eeaf1d3e6a7520cb7cd3c1088a017cecfd43a14f
Josh Blum <josh@joshknows.com>
2017-07-24 22:31:37 -0500

    pothos-blocks-0.4.4

--- pothosblocks.rb
+++ pothosblocks.rb
@@ -2,8 +2,8 @@ class Pothosblocks < Formula
   desc "Pothos core processing blocks"
   homepage "https://github.com/pothosware/pothos-blocks/wiki"
   head "https://github.com/pothosware/pothos-blocks.git"
-  url "https://github.com/pothosware/pothos-blocks/archive/pothos-blocks-0.4.3.tar.gz"
-  sha256 "96d1ae6722f92df42ea53c268c8fd1ce5276ffe2b928065055faa8a066e060c6"
+  url "https://github.com/pothosware/pothos-blocks/archive/pothos-blocks-0.4.4.tar.gz"
+  sha256 "b995e5aae1de3d7fe3f7f92cf2d8fbbb47052991842bdb8e89ef0c0cb9c737f1"
 
   depends_on "cmake" => :build
   depends_on "pothos"
