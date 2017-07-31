438b4118f80c9699efdb7e5acddf40ed9929549e
Josh Blum <josh@joshknows.com>
2017-07-31 09:16:32 -0500

    soapy-remote-0.4.2

--- soapyremote.rb
+++ soapyremote.rb
@@ -2,8 +2,8 @@ class Soapyremote < Formula
   desc "Use any Soapy SDR remotely"
   homepage "https://github.com/pothosware/SoapyRemote/wiki"
   head "https://github.com/pothosware/SoapyRemote.git"
-  url "https://github.com/pothosware/SoapyRemote/archive/soapy-remote-0.4.1.tar.gz"
-  sha256 "ea83cdfb4c8eb4dadc222e98138c91e3df543ec8b65958b8131f516574924403"
+  url "https://github.com/pothosware/SoapyRemote/archive/soapy-remote-0.4.2.tar.gz"
+  sha256 "84242c097473ee95adbd919538c597ebb4773bcb03973d56c862567ba8eeffec"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
