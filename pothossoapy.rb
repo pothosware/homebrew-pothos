d7ae8737c62094e257d8f99b896f17119b43955e
Josh Blum <josh@joshknows.com>
2017-12-31 00:22:43 -0600

    pothossoapy - nlohmann_json dependency

--- pothossoapy.rb
+++ pothossoapy.rb
@@ -8,6 +8,7 @@ class Pothossoapy < Formula
   depends_on "cmake" => :build
   depends_on "pothos"
   depends_on "poco"
+  depends_on "nlohmann/json/nlohmann_json"
   depends_on "soapysdr"
 
   def install
