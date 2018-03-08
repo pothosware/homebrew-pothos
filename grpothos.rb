fac1449086a07a18dbb59ad444e7a83feceada88
Josh Blum <josh@joshknows.com>
2018-03-07 20:18:48 -0600

    added gr-pothos, python tweaks pothos-liquiddsp

--- grpothos.rb
+++ grpothos.rb
@@ -1,10 +1,10 @@
-class Pothosliquiddsp < Formula
-  desc "Pothos liquiddsp processing blocks"
-  homepage "https://github.com/pothosware/PothosLiquidDSP/wiki"
-  head "https://github.com/pothosware/PothosLiquidDSP.git"
+class Grpothos < Formula
+  desc "Pothos bindings for gnuradio processing blocks"
+  homepage "https://github.com/pothosware/gr-pothos/wiki"
+  head "https://github.com/pothosware/gr-pothos.git"
 
   depends_on "cmake" => :build
-  depends_on "liquid-dsp" => :build
+  depends_on "gnuradio"
   depends_on "pothos"
 
   resource "ply" do
@@ -12,16 +12,10 @@ class Pothosliquiddsp < Formula
     sha256 "96e94af7dd7031d8d6dd6e2a8e0de593b511c211a86e28a9c9621c275ac8bacb"
   end
 
-  resource "pyyaml" do
-    url "https://pypi.python.org/packages/4a/85/db5a2df477072b2902b0eb892feb37d88ac635d36245a72a6a69b23b383a/PyYAML-3.12.tar.gz"
-    sha256 "592766c6303207a20efc445587778322d7f73b161bd994f227adaa341ba212ab"
+  resource "cheetah" do
+    url "https://pypi.python.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz"
+    sha256 "be308229f0c1e5e5af4f27d7ee06d90bb19e6af3059794e5fd536a6f29a9b550"
   end
-
-  resource "mako" do
-    url "https://pypi.python.org/packages/eb/f3/67579bb486517c0d49547f9697e36582cd19dafb5df9e687ed8e22de57fa/Mako-1.0.7.tar.gz"
-    sha256 "4e02fde57bd4abb5ec400181e4c314f56ac3e49ba4fb8b0d50bba18cb27d25ae"
-  end
-
 
   def install
 
@@ -30,7 +24,7 @@ class Pothosliquiddsp < Formula
     args += ["-DPYTHON_EXECUTABLE=/usr/bin/python2.7"]
 
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
-    res = %w[ply pyyaml mako]
+    res = %w[ply cheetah]
     res.each do |r|
       resource(r).stage do
         system "python2.7", *Language::Python.setup_install_args(libexec/"vendor")
