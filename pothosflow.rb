5f1e620c24e1d36febdb4ef165a559cd2e0b0a4c
Josh Blum <josh@joshknows.com>
2018-04-28 13:38:22 -0500

    pothos-flow-0.6.1

--- pothosflow.rb
+++ pothosflow.rb
@@ -2,8 +2,8 @@ class Pothosflow < Formula
   desc "Pothos graphical design tool"
   homepage "https://github.com/pothosware/PothosFlow/wiki"
   head "https://github.com/pothosware/PothosFlow.git"
-  url "https://github.com/pothosware/PothosFlow/archive/pothos-flow-0.6.0.tar.gz"
-  sha256 "2f759c1fcdd5bb4e6318d8d20fa43e0790d6c8fb54e6525069382cbddfd66a47"
+  url "https://github.com/pothosware/PothosFlow/archive/pothos-flow-0.6.1.tar.gz"
+  sha256 "a397f4a524fd45e490b008fd2d998ce909b4b1ece2c61412cdc88e0bbe5002ea"
 
   depends_on "cmake" => :build
   depends_on "pothos"
