37f4ac3aa207e152767ae96a32e9627e59ae8c63
Josh Blum <josh@joshknows.com>
2018-05-10 19:52:01 -0500

    soapy-hackrf-0.3.3

--- soapyhackrf.rb
+++ soapyhackrf.rb
@@ -2,8 +2,8 @@ class Soapyhackrf < Formula
   desc "Soapy SDR plugin for Hack RF"
   homepage "https://github.com/pothosware/SoapyHackRF/wiki"
   head "https://github.com/pothosware/SoapyHackRF.git"
-  url "https://github.com/pothosware/SoapyHackRF/archive/soapy-hackrf-0.3.2.tar.gz"
-  sha256 "1f96103e9390f059e469b537c9f697fe998df10c78504f531408626023dc81fd"
+  url "https://github.com/pothosware/SoapyHackRF/archive/soapy-hackrf-0.3.3.tar.gz"
+  sha256 "7b24a47cee42156093bf82982b4fc6184a7c86101c3b8ee450274e57ee1c4b90"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
