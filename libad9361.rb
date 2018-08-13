919654805cc52171979172cf9d8f5c18a30e2c09
Emmanuel Blot <emmanuel.blot@free.fr>
2018-08-13 15:13:19 +0200

    Add various libraries and frameworks for the Adalm-Pluto SDR from Analog Devices

---
+++ libad9361.rb
@@ -0,0 +1,33 @@
+class Libad9361 < Formula
+  desc "IIO AD9361 library for filter design and handling, multi-chip sync, etc."
+  homepage "https://wiki.analog.com/software/linux/docs/iio/iio"
+  url "https://github.com/analogdevicesinc/libad9361-iio/archive/v0.1.tar.gz"
+  sha256 "46eeacb696e3b70873c541761af189a8ecde6ab7b3e7a5273dfc003e3ba0165d"
+  head "https://github.com/analogdevicesinc/libad9361-iio.git"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "libiio"
+
+  def install
+    libiio = Formulary.factory 'libiio'
+
+    mktemp do
+      if not build.head?
+        inreplace  buildpath/"CMakeLists.txt",
+                   "include(FindGit OPTIONAL)",
+                   "SET(FindGit)"
+      end
+      inreplace  buildpath/"CMakeLists.txt",
+                 "FRAMEWORK DESTINATION lib",
+                 "FRAMEWORK DESTINATION ."
+
+      system "cmake", "-G", "Ninja", buildpath, "-DOSX_PACKAGE=OFF",
+             "-DCMAKE_FRAMEWORK_PATH=#{libiio.opt_prefix}",
+             *std_cmake_args
+      system "ninja"
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+
+end
---
+++ soapyplutosdr.rb
@@ -0,0 +1,26 @@
+class Soapyplutosdr < Formula
+  desc "Soapy SDR plugin for PlutoSDR."
+  homepage "https://github.com/jocover/SoapyPlutoSDR"
+  head "https://github.com/jocover/SoapyPlutoSDR.git"
+
+  depends_on "soapysdr"
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+  depends_on "libiio"
+  depends_on "libad9361"
+  depends_on "soapysdr"
+
+  def install
+    libiio = Formulary.factory 'libiio'
+    libad9361 = Formulary.factory 'libad9361'
+
+    mktemp do
+      system "cmake", "-G", "Ninja", buildpath,
+             "-DCMAKE_FRAMEWORK_PATH=#{libad9361.opt_prefix};#{libiio.opt_prefix}",
+             *std_cmake_args
+      system "ninja"
+      system "cmake", "--build", ".", "--target", "install"
+    end
+  end
+
+end
