2ff7760772dff96f4a65ac1947df470b08281b8f
Josh Blum <josh@joshknows.com>
2018-08-14 09:31:22 -0500

    depend on spuce from audiofilter tap

--- pothoscomms.rb
+++ pothoscomms.rb
@@ -9,7 +9,7 @@ class Pothoscomms < Formula
   depends_on "pothos"
   depends_on "poco"
   depends_on "nlohmann/json/nlohmann_json"
-  depends_on "pothosware/pothos/spuce"
+  depends_on "audiofilter/spuc/spuce"
 
   def install
     mkdir "build" do
