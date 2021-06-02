3bb77381f9c4bfbaa86c9ea641b7ae445ce56d89
Josh Blum <josh@joshknows.com>
2021-06-02 08:56:04 -0500

    airspy and hf tags for 0.2.0

--- soapyairspy.rb
+++ soapyairspy.rb
@@ -2,8 +2,8 @@ class Soapyairspy < Formula
   desc "Soapy SDR plugins for Airspy"
   homepage "https://github.com/pothosware/SoapyAirspy/wiki"
   head "https://github.com/pothosware/SoapyAirspy.git"
-  url "https://github.com/pothosware/SoapyAirspy/archive/soapy-airspy-0.1.2.tar.gz"
-  sha256 "3c0bafd27063df1cbc7913c0b5d3d1d9a0624d25d290f0818261ae52d0f29ee8"
+  url "https://github.com/pothosware/SoapyAirspy/archive/soapy-airspy-0.2.0.tar.gz"
+  sha256 "4279ab4278fab699ef8325f3f921b2307496130a56028d33022be10916b6ccff"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
--- soapyairspyhf.rb
+++ soapyairspyhf.rb
@@ -2,8 +2,8 @@ class Soapyairspyhf < Formula
   desc "Soapy SDR plugins for AirspyHF+"
   homepage "https://github.com/pothosware/SoapyAirspyHF/wiki"
   head "https://github.com/pothosware/SoapyAirspyHF.git"
-  url "https://github.com/pothosware/SoapyAirspyHF/archive/soapy-airspyhf-0.1.0.tar.gz"
-  sha256 "8029925abbda64518947c72006afa156f24dc461d2486c4333f5b8e11463514d"
+  url "https://github.com/pothosware/SoapyAirspyHF/archive/soapy-airspyhf-0.2.0.tar.gz"
+  sha256 "f134ddc491151dfeb1ed9ca5a614d207d493b89fef173b325ce9e6f4e07884b3"
 
   depends_on "cmake" => :build
   depends_on "soapysdr"
